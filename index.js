89f891b65c55a5144fba79dc50cd6ecbad453c62
dctrl <>
2016-05-21 22:55:34 -0700

    Blue/Red Ambience

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 const Sidewalk = require('./SidewalkDrawer')
-const colours = [
-  [244, 30, 50],
-  [200, 100, 200],
-  [233, 40, 100],
-  [200, 60, 120],
-  [255, 100, 0]
-];
-
-const bcolours = [
-  [20, 30, 250],
-  [20, 100, 200],
-  [20, 232, 0],
-  [20, 255, 144],
-  [20, 100, 200]
-];
 
 const sidewalk = new Sidewalk(13,62)
-sidewalk.fullFill([255,0,0], 1)
+sidewalk.fullFill([0,0,255], 1)
 
 point = {x: 10, y:10}
 
-sidewalk.drawPoint([0,0,255], point)
+var colours = [
+  [255,0,0],
+  [0,0, 255]
+];
+var i = 0
+setInterval( ()=>{
+  sidewalk.fullFill(colours[i%2], 1);
+  i++;
+}, 12000);
