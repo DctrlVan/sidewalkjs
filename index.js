82988344ecd865a6c411fbfef3be4d6eeb3003e0
pirate <pirate>
2016-06-23 02:36:23 -0700

    !~~

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ function getRandomInt (min, max) {
 
 setInterval( () => {
   for (i = 0; i < 62; i++) {
-    for (y = 0; y < 13; y++) {  
+    for (y = 0; y < 13; y++) {
       var randColour = [0, getRandomInt(1, 255), getRandomInt(1, 255)];
       sidewalk.drawPoint(randColour, {'x': y, 'y': i});
     }
   }
   sidewalk.update();
 }, 500);
+
+function delayBall(colour, x){
+  return setTimeout(()=>{
+    sidewalk.bounceBall(colour)
+  },x);
+}
+
+
+let red = [255,0,0]
+let i = 0
+while(i<5){
+  delayBall(red, 5000*i)
+  i++
+}
