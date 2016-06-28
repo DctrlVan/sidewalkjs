053e2ec3ddf13b52e6b8ba9e071de7cbc121195a
dctrl <>
2016-06-27 20:17:24 -0700

    Implement random method

--- SidewalkConnector.js
+++ SidewalkConnector.js
@@ -5,27 +5,25 @@ const stream = createOPCStream();
 const socket = new Socket().setNoDelay().connect(7890, '192.168.1.230');
 stream.pipe(socket);
 
-
 class SidewalkConnector {
-  constructor(width, height){
+  constructor (width, height) {
     this.width = width;
     this.height = height;
     this.strand = createStrand(width * height);
     this.columns = this._createColumns();
-    console.log("Constructed Sidewalk Controller Class into Object",
-      {width, height});
+    console.log("Constructed Sidewalk Controller Class into Object", {width, height});
   }
 
-  _createColumns(){
+  _createColumns () {
     var columns = [];
     var i = 0; while (i < this.width) {
       columns.push(this.strand.slice(this.height * i, this.height * (i + 1)));
       i++;
     }
-    return columns
+    return columns;
   }
 
-  update(){
+  update () {
     stream.writePixels(0, this.strand.buffer);
   }
 }
--- index.js
+++ index.js
@@ -1,31 +1,4 @@
 const Sidewalk = require('./SidewalkDrawer')
-const sidewalk = new Sidewalk(13,62);
-const Kefir = require('kefir')
+const sidewalk = new Sidewalk(13, 62);
 
-function getRandomInt (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-Kefir.fromPoll(500, () => {
-  return Array.from(Array(62)).map( () => Array.from(Array(13)))
-})
-.map(arr => {
-  return arr.map(sub => sub.map( () => [0, getRandomInt(1, 255), getRandomInt(1, 255)]))
-})
-.onValue(arr => {
-  arr.forEach( (sub, y) => sub.forEach( (color, x) => sidewalk.drawPoint(color, {x,y})))
-  sidewalk.update();
-})
-
-function delayBall (colour, x) {
-  return setTimeout( () => {
-    sidewalk.bounceBall(colour)
-  }, x);
-}
-
-let red = [255, 255, 255]
-let i = 0
-while(i < 1) {
-  delayBall(red, 10000 * i)
-  i++
-}
+sidewalk.random(0, true, true, 1250);
